f1bda7e476035aeae4322fb23fb252964dc75d6a
Fernando <el.zoona@gmail.com>
2015-11-16 17:41:53 -0300

    Reorganizing the code

--- colores.py
+++ colores.py
@@ -1,21 +1,60 @@
 import pygame, sys
 import random
 from pygame.locals import * #This imports constants like QUIT
-pygame.init()
-surface = pygame.display.set_mode((1280,720), 1, 32)
-pygame.display.set_caption('Colores')
-pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096) #Check for better values
+import glob
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.mixer.quit()
-            pygame.quit()
-            sys.exit()
+FPS = 30
+SCREENWIDTH = 1280
+SCREENHEIGHT = 720
 
-        if event.type == KEYDOWN:
-            surface.fill((random.randrange(0,255,1),random.randrange(0,255,1),random.randrange(0,255,1)))
-            pygame.mixer.Sound("sounds/"+str(random.randrange(1,66,1))+".wav").play() #FIXME some latency problems
 
+
+
+
+
+cat = pygame.image.load('img/stand01.png')
+catX=10
+catY=10
+
+def loadSound():
+    pygame.mixer.pre_init(frequency=22050, size=-16, channels=2, buffer=4096) #pre init to avoid lag
+    pygame.mixer.init()
+    soundLibrary = glob.glob('sounds/*.wav')
+    samples = []
+    for item in soundLibrary:
+        samples.append(pygame.mixer.Sound(item))
+    return samples
+
+
+
+def main():
+    global fpsClock, surface
+
+    pygame.init()
+    fpsClock = pygame.time.Clock()
+    surface = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT), 1, 32)
+
+    pygame.display.set_caption('Colores')
+    samples=loadSound()
+
+
+    while True:
+
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.mixer.quit()
+                pygame.quit()
+                sys.exit()
+
+            if event.type == KEYDOWN:
+                surface.fill((random.randrange(0,255,1),random.randrange(0,255,1),random.randrange(0,255,1)))
+                surface.fill((random.randrange(0,255,1),random.randrange(0,255,1),random.randrange(0,255,1)))
+                samples[(random.randrange(0,len(samples),1))].play() #Kind of spaguetti, but allows to add more sounds without toching the code
+                surface.blit(cat, (catX, catY))
 
         pygame.display.update()
+        fpsClock.tick(FPS)
+
+if __name__ == '__main__':
+    main()
